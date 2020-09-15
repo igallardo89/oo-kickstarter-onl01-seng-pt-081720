8bc58006529c8675f72643ceb633f830f6dd0a3e
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-15 01:49:32 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,8 +9,7 @@ class Backer
     end 
    def back_project(project)
    @backed_projects << project
-   @backed_projects << Project
-   @backed_projects
+    project.backers << self
    end 
   
    
